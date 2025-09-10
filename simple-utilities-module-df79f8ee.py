916503879d0ffa15b59a90081eb232c2a65f6679
monke-prog <monke@airweave.ai>
2025-09-10 13:22:55 +0200

    Update monke test file: simple-utilities-module-df79f8ee.py

--- simple-utilities-module-df79f8ee.py
+++ simple-utilities-module-df79f8ee.py
@@ -1,16 +1,16 @@
-"""A tiny module with a sum function and a greeter class. Includes the token in a docstring. | token="""
+"""A tiny Python module with a simple function and a simple class. | token="""
 
 def add(a, b):
     """Return the sum of a and b. Token: df79f8ee"""
     return a + b
 
 
-class Greeter:
-    """Simple greeter class."""
-    def __init__(self, name):
-        self.name = name
-
-    def greet(self):
-        """Return a greeting message."""
-        return f"Hello, {self.name}!"
+class Counter:
+    """A simple counter that can be incremented."""
+    def __init__(self, start=0):
+        self.value = start
+    def increment(self, step=1):
+        """Increase the counter by step and return the new value."""
+        self.value += step
+        return self.value
 
